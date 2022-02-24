88e57b95434da66210084fa0d638ad2fe22f995d
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-02-24 17:48:14 +0500

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,13 +78,14 @@ $modalAdd.on('click', function (event) {
     event.preventDefault();
     console.log(event.target == $modalAdd ? $modalAdd : '')
     if(event.target = $modalAdd) {
-        anime.timeline({
-            targets: $modalAdd,
-            opacity: opacity
-        }).add({
-            scale: 0,
-            easing: easing
-        })
+        // anime.timeline({
+        //     targets: $modalAdd,
+        //     opacity: opacity
+        // }).add({
+        //     scale: 0,
+        //     easing: easing
+        // })
+        alert('s')
     } else {alert()}
     // $modalAdd.css('display', 'none')
 })
