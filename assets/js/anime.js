d45f42037cbb6fc1ad9880e455c7a6dccaf3fa59
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-02-24 16:02:19 +0500

    ...

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1308,4 +1308,3 @@ anime.timeline = timeline;
 anime.easing = parseEasings;
 anime.penner = penner;
 anime.random = function (min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; };
-export default anime  
