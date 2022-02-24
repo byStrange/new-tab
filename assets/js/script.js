b57bb9a49f59224a0d2bd3a16e69e4f1b91b083a
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-02-24 15:43:38 +0500

    first js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log('happy mo :|')
+
+function date() {
+    const now = new Date();
+    return {
+        hour: now.getHours().toString().length == 1 ? "0"+ now.getHours(): now.getHours(),
+        minute: now.getMinutes().toString().length == 1 ? '0'+  now.getMinutes() : now.getMinutes(),
+        second: now.getSeconds(),
+        month: now.getMonth(),
+        day:  now.getDay()
+    }
+}
+
+
+every().second(()=>{
+    $hour.text(`${date().hour}:${date().minute}`)
+    $hour.setAttribute('date-second', date().second)
+    $d.text('')
+})
